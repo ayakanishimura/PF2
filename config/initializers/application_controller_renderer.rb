415b7436631246358be92bc50f48bbe79767cca6
ayakanishimura <a.tktk08@gmail.com>
2021-01-10 08:26:25 +0000

    [add]ユーザー機能・投稿機能

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'e10054ceff1adcf8af8c306c085de3a8c611bac3bfa7458cd1df3013d63f5bab21f92673eacbec496def096efd92fe6fbcc8f5aa2265feddd04a35c948caea30'
