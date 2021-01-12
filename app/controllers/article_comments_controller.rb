bcdc966a52d7351eaed2c6a5c058f9afb7dfe7a8
ayakanishimura <a.tktk08@gmail.com>
2021-01-12 23:49:50 +0000

    [add]コメント非同期通信

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -6,12 +6,12 @@ class ArticleCommentsController < ApplicationController
     comment.user_id = current_user.id
     comment.article_id = article.id
     comment.save
-    redirect_to article_path(article)
+    # redirect_to article_path(article)
   end
 
   def destroy
     ArticleComment.find_by(id: params[:id], article_id: params[:article_id]).destroy
-    redirect_to article_path(params[:article_id])
+    # redirect_to article_path(params[:article_id])
   end
 
   private
