bcff4043cf46e801cac8b1358a562850a786cf48
ayakanishimura <a.tktk08@gmail.com>
2021-01-14 07:19:41 +0000

    [add]マイページレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
-  
+
   def follows
     user = User.find(params[:id])
     @users = user.followings
