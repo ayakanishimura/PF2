65d7ef506e8150af2ada29a48f91dccd52a21e86
ayakanishimura <a.tktk08@gmail.com>
2021-01-17 12:06:32 +0000

    [update]いいねランキング機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,24 @@ class ArticlesController < ApplicationController
     @articles = Article.page(params[:page]).reverse_order
     @genres = Genre.all
     #いいねランキング
-    @all_ranks = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
+    # @all_ranks = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
+
+    # today = Time.zone.today
+    # articles = Article.where(start_time: today.beginning_of_month..today.end_of_month)
+    # article_ids = Favorite
+    #   .where(article_id: articles.pluck(:id))
+    #   .group(:article_id)
+    #   .order('count(article_id) desc').limit(5).pluck(:article_id)
+    # @all_ranks = Article.where(id: article_ids)
+
+    # one_months_ago = 1.months.ago
+    # one_months_ago_articles = Article.where(start_time: one_months_ago.beginning_of_month..one_months_ago.end_of_month)
+    # one_months_ago_article_ids = Favorite
+    #   .where(article_id: one_months_ago_articles.pluck(:id))
+    #   .group(:article_id)
+    #   .order('count(article_id) desc').limit(5).pluck(:article_id)
+    # @one_months_ago_ranks = Article.where(id: one_months_ago_article_ids)
+
   end
 
   def show
