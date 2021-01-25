5a5e53b9026b9f5d39ba1d3ae27e1553c284b310
ayakanishimura <a.tktk08@gmail.com>
2021-01-25 12:33:27 +0000

    [update]レイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,6 @@ class User < ApplicationRecord
     def self.guest
       find_or_create_by!(email: 'guest@example.com') do |user|
         user.password = SecureRandom.urlsafe_base64
-        user.confirmed_at = Time.now  
       end
     end
 
