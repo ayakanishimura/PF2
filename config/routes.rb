5c712e0003198ecca12e1cadf3756abfb14119f2
ayakanishimura <a.tktk08@gmail.com>
2021-01-26 11:39:15 +0000

    [uodate]背景レイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/search' => 'search#search'
   get '/genre_search' => 'search#genre_search'
-  devise_for :users
   root to: 'homes#top'
   get '/about' => 'homes#about'
   resources :articles do
